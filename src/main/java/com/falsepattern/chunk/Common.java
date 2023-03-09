32c32df053363a7bb276f040f146805b8b4abb44
FalsePattern <30945458+FalsePattern@users.noreply.github.com>
2023-03-09 20:06:44 +0100

    Initial Commit

---
+++ src/main/java/com/falsepattern/chunk/Common.java
@@ -0,0 +1,11 @@
+package com.falsepattern.chunk;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Non-minecraft stuff to avoid accidental classloading in spaghetti code
+ */
+public class Common {
+    public static final Logger LOG = LogManager.getLogger(Tags.MODID);
+}
