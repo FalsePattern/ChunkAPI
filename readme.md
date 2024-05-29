045179557689d6da9c5e3cd5e7e5f5da9876fbbf
FalsePattern <me@falsepattern.com>
2024-05-29 15:41:00 +0200

    FOSS

--- readme.md
+++ readme.md
@@ -1,12 +1,54 @@
 # ChunkAPI
-
+<a rel="license" href="https://www.gnu.org/licenses/agpl-3.0.en.html"><img alt="AGPLv3 logo" style="border-width:0" src="https://www.gnu.org/graphics/agplv3-with-text-162x68.png" /></a>
 ## Author: FalsePattern
-## License: CC-BY-NC-ND 4.0 <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a>
+## License: GNU AGPLv3.0 with additional permissions (see below, or in the LICENSE file)
 
 Summary
 -------
 
 A mod for adding custom data to chunks without the hassle of writing custom packets, event handling, loading hooks, and more.
+
+License
+-------
+
+ChunkAPI is designed with players in mind, and as such, it uses slightly aggressive licensing to make sure that third
+party developers cannot break your worlds in an irrecoverable way by changing huge parts of the API.
+
+For this reason, the mod itself is fully licensed under the GNU Affero General Public License, version 3.0 or later,
+with the following additional permissions to allow it to be easily used by modpacks and other mods:
+
+<details>
+<summary>License excerpt</summary>
+
+```
+According to section 7 of the GNU AGPL, this software comes with the following additional permissions:
+
+You may link this mod against proprietary code developed by Mojang Studios (Minecraft and its dependencies), as well
+as distribute binary builds of it as part of a "Minecraft modpack" without the AGPL restrictions spreading to any other
+files in said modpack.
+
+You may also develop any mods that depend on classes the API packages (`com.falsepattern.chunk.api.*`) and distribute that
+mod under terms of your own choice. Note that this permissions does not apply to anything outside the internal package,
+as those are internal implementations of ChunkAPI and are not meant to be used in any external mod, thus the full force
+of the AGPL applies in such cases.
+
+These additional permissions get removed if you modify, extend, rename, remove, or in any other way alter
+any part of the public API code, save data binary formats, or the network protocol in such a way that makes
+it not perfectly compatible with the official versions of ChunkAPI.
+
+If you wish to make modifications to the previously mentioned features, please create a pull request on the official
+release of ChunkAPI with full reasoning and specifications behind the requested change, and once it's merged and
+published in an official release, your modified version may once again inherit this additional permission.
+
+To avoid abuse caused by upstream API changes, these permissions are valid as long as your modified version of ChunkAPI
+is perfectly compatible with any public release of the official ChunkAPI.
+```
+
+</details>
+
+Note that while the mentioned limitations might seem unnecessary, it is essential for protecting your saves from getting
+"trapped" in a random fork of ChunkAPI without any way to recover to vanilla or the official release of ChunkAPI
+without world corruption, in case no suitable backups are available.
 
 Goals
 -----
--- src/main/java/com/falsepattern/chunk/api/package-info.java
+++ src/main/java/com/falsepattern/chunk/api/package-info.java
@@ -1,11 +1,51 @@
 /*
- * Copyright (c) 2023 FalsePattern
- * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/
- * or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ * ChunkAPI
+ *
+ * Copyright (C) 2023-2024 FalsePattern
+ * All Rights Reserved
+ *
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions of the Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------------
+ * According to section 7 of the GNU AGPL, this software comes with the following additional permissions:
+ *
+ * You may link this mod against proprietary code developed by Mojang Studios (Minecraft and its dependencies), as well
+ * as distribute binary builds of it as part of a "Minecraft modpack" without the AGPL restrictions spreading to any other
+ * files in said modpack.
+ *
+ * You may also develop any mods that depend on classes the API packages (`com.falsepattern.chunk.api.*`) and distribute that
+ * mod under terms of your own choice. Note that this permissions does not apply to anything outside the internal package,
+ * as those are internal implementations of ChunkAPI and are not meant to be used in any external mod, thus the full force
+ * of the AGPL applies in such cases.
+ *
+ * These additional permissions get removed if you modify, extend, rename, remove, or in any other way alter
+ * any part of the public API code, save data binary formats, or the network protocol in such a way that makes
+ * it not perfectly compatible with the official versions of ChunkAPI.
+ *
+ * If you wish to make modifications to the previously mentioned features, please create a pull request on the official
+ * release of ChunkAPI with full reasoning and specifications behind the requested change, and once it's merged and
+ * published in an official release, your modified version may once again inherit this additional permission.
+ *
+ * To avoid abuse caused by upstream API changes, these permissions are valid as long as your modified version of ChunkAPI
+ * is perfectly compatible with any public release of the official ChunkAPI.
  */
 
 /**
+ * This is an API package covered by the additional permissions in the license.
+ * <p>
  * This package contains the classes that you will use to interact with the ChunkAPI library.
  *
  * @since 0.5.0
